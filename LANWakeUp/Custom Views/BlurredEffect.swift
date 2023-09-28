efb7043b66ada23b75de1d2503aaa05440d56c99
Svaarich <svaarich@gmail.com>
2023-09-28 23:28:33 +0300

    Change blurred mode.

--- LANWakeUp/Custom Views/BlurredEffect.swift
+++ LANWakeUp/Custom Views/BlurredEffect.swift
@@ -5,7 +5,7 @@ import SwiftUI
 struct BlurredEffect: NSViewRepresentable {
     func makeNSView(context: Context) -> some NSView {
         let view = NSVisualEffectView()
-        view.material = .sidebar
+        view.material = .fullScreenUI
         view.blendingMode = .behindWindow
         return view
     }
