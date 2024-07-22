91cb1e91d77e9265a85c1aa7641bfe67bcd68db4
Danylo <svaarich@gmail.com>
2024-07-22 15:52:08 +0300

    add ping func

--- app/Anywakey_MacOS/Services/Network/Network.swift
+++ app/Anywakey_MacOS/Services/Network/Network.swift
@@ -19,23 +19,23 @@ public class Network {
     }
     
     // Ping selected host / IP address and returns
-//    func ping(address: String, onDone: @escaping (_ ping: Double, _ isAccessible: Bool) -> Void) {
-//        // if address is empty returns false
-//        guard
-//            address.isValidAdress()
-//        else { return onDone(0, false) }
-//            let ones = try? SwiftyPing(host: address,
-//                                       configuration: PingConfiguration(interval: 0.5, with: 2),
-//                                       queue: DispatchQueue.global())
-//            ones?.observer = { responce in
-//                let isSuccess = responce.error == nil
-//                DispatchQueue.main.async {
-//                    onDone(responce.duration, isSuccess)
-//                }
-//            }
-//            ones?.targetCount = 1
-//            try? ones?.startPinging()
-//    }
+    func ping(address: String, onDone: @escaping (_ ping: Double, _ isAccessible: Bool) -> Void) {
+        // if address is empty returns false
+        guard
+            address.isValidAddress()
+        else { return onDone(0, false) }
+            let ones = try? SwiftyPing(host: address,
+                                       configuration: PingConfiguration(interval: 0.5, with: 2),
+                                       queue: DispatchQueue.global())
+            ones?.observer = { responce in
+                let isSuccess = responce.error == nil
+                DispatchQueue.main.async {
+                    onDone(responce.duration, isSuccess)
+                }
+            }
+            ones?.targetCount = 1
+            try? ones?.startPinging()
+    }
 }
 
 
