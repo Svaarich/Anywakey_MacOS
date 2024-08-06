062923d5c368d39be1f984296c1984f580e04220
Danylo <svaarich@gmail.com>
2024-08-06 16:31:46 +0300

    app info bubble button

--- app/Anywakey_MacOS/Core/AppInfoView.swift
+++ app/Anywakey_MacOS/Core/AppInfoView.swift
@@ -21,25 +21,11 @@ struct AppInfoView: View {
     }
     
     private var dismissButton: some View {
-        Button {
-            withAnimation(.spring(duration: 0.3)) {
-                showView = false
-            }
+        BubbleButton {
+            showView = false
         } label: {
-//            Image(systemName: "xmark")
             Text("close")
-                .foregroundStyle(hoverClose ? .white : .secondary)
-                .padding(.horizontal, 8)
-                .frame(height: 20)
-                .background(Color.gray.opacity(hoverClose ? 0.4 : 0.2))
-                .clipShape(RoundedRectangle(cornerRadius: 5))
         }
-        .buttonStyle(.borderless)
-        .scaleEffect(hoverClose ? 1.1 : 1.0)
-        .onHover { hover in
-            withAnimation(.spring(duration: 0.3)) {
-                hoverClose = hover
-            }
-        }
+
     }
 }
